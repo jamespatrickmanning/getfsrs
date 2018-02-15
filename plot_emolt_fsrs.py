a15f552bb4a8d65d54b56383fe56548bde361119
jamespatrickmanning <james.manning@noaa.gov>
2018-02-15 14:51:55 -0500

    JiM suggestions with some "hardcodes"
    reduced lat/lon, changed some colors, reworded "eMOLT"

--- plot_emolt_fsrs.py
+++ plot_emolt_fsrs.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 14 12:45:00 2018
-
-@author: zdong
+makes map of both eMOLT and FSRS sites >1 year
+@author: Xiaoxu
+Modifications by JiM and Xiaoxu
 """
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
@@ -29,9 +30,13 @@ def dm2dd(lat,lon):
         dd=float(d)
         lon_value=cc+(dd/60.)
     return lat_value, -lon_value
+#### HARDCODES  #####
 case=''#'_MAB'
-df=pd.read_csv('sqldump_sites'+case+'.dat',index_col=2,delim_whitespace=True)
-dfh=pd.read_csv('fsrs_sites.csv')
+projec='merc'
+gbox=[-74,-58,39,48]
+#####################
+df=pd.read_csv('sqldump_sites'+case+'.dat',index_col=2,delim_whitespace=True)# eMOLT sites > 1year from JiM
+dfh=pd.read_csv('fsrs_sites.csv') # getfsrs.py output where sites are > 1year and with 1km
 fig = plt.figure()
 a=fig.add_subplot(1,1,1)
 # emolt site
@@ -43,22 +48,22 @@ for k in range(len(df)):
       [la,lo]=dm2dd(la,lo)
       Lon.append(lo)
       Lat.append(la)
-my_map = Basemap(projection='lcc', lat_0 = 45, lon_0 = -63,
+my_map = Basemap(projection=projec,
     resolution = 'h', area_thresh = 0.3,
-    llcrnrlon=-75, llcrnrlat=38.0,
-    urcrnrlon=-55, urcrnrlat=50) 
+    llcrnrlon=gbox[0], llcrnrlat=gbox[2],
+    urcrnrlon=gbox[1], urcrnrlat=gbox[3]) 
 my_map.drawcoastlines()
 my_map.drawcountries()
-my_map.fillcontinents(color = 'coral')
+my_map.fillcontinents(color = 'grey')
 my_map.drawmapboundary() 
 x,y=my_map(Lon,Lat)
 my_map.plot(x, y, 'bo', markersize=4)
 x,y=my_map(dfh['Longitude'].values,dfh['Latitude'].values)
 my_map.plot(x, y, 'go', markersize=4)
-label=["Emolt","Fsrs"]
+label=["eMOLT","FSRS"]
 a.legend(label,loc="lower right")
-a.set_title('Emolt sites(>1 year) and Fsrs sites(>1 year)',fontsize=15)
+a.set_title('eMOLT sites (>1 year) and FSRS sites (>1 year)',fontsize=15)
 my_map.drawparallels(np.arange(30,80,3),labels=[1,0,0,0])
 my_map.drawmeridians(np.arange(-180,180,3),labels=[1,1,0,1])
-plt.savefig("plot emolt sites(>1 year) and fsrs sites(>1 year).png")
+plt.savefig("plot_eMOLT_FSRS_more_than_1_year.png")
 plt.show()
